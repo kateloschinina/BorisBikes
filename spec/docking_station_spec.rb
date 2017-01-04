7556be61393157246b8f2e40cd83016479e59bfb
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro.local>
2017-01-04 18:23:35 +0000

    Makes sure that release_bike is returning a Bike and that working? method is working for this bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,16 @@ require 'docking_station'
 
 describe DockingStation do
 
-  #it 'Check if release_bike returns nil' do
-    it { is_expected.to respond_to :release_bike }
-  #end
+  it { should respond_to :release_bike }
+
+  docking_station = DockingStation.new
+
+  it 'Gets a bike' do
+    docking_station.release_bike.should be_a(Bike)
+  end
+
+  it 'Whether bike is working' do
+    docking_station.release_bike.should respond_to(:working?)
+  end
+
 end
