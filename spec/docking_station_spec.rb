e642e0004f9b800dc802974fe54419c65ec4db7d
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro-2.local>
2017-01-05 11:51:54 +0000

    Final adjustments

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,14 +4,19 @@ describe DockingStation do
 
   it { should respond_to :release_bike }
 
-  docking_station = DockingStation.new
-
-  it 'Gets a bike' do
-    docking_station.release_bike.should be_a(Bike)
-  end
-
-  it 'Whether bike is working' do
-    docking_station.release_bike.should respond_to(:working?)
+  it 'releases working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
 
 end
+
+=begin
+it 'Gets a bike' do
+  docking_station.release_bike.should be_a(Bike)
+end
+
+it 'Whether bike is working' do
+  docking_station.release_bike.should respond_to(:working?)
+end
+=end
